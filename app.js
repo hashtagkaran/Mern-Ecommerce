8c44c288cccbcca97489f5968faa4ce2d4c58442
Karan Singh <thekrsingh@gmail.com>
2020-06-06 16:49:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+// my routes
+
+const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
+const categoryroutes = require("./routes/category");
+
+// mongoose.connect("mongodb://localhost:27017/test", {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// });
+
+mongoose
+	.connect(process.env.DATABASE, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.log("DB CONNECTED");
+	})
+	.catch(console.log("DB GOT OPPSSSS"));
+
+// MiddleWares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+// routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryroutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+	console.log(`appp is running at ${port}`);
+});
